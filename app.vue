c420e7281f9ff004a4a50367d2a3d137235e2ebe
Chris Leung <pserleung@gmail.com>
2023-07-07 20:46:26 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-main class="mt-15">
+      <v-container>
+        <v-btn>Test</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import vuetify from 'vite-plugin-vuetify'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  hooks: {
+    'vite:extendConfig': (config) => {
+      config.plugins!.push(vuetify())
+    },
+  },
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+  css: ['@/assets/style/main.scss'],
+})
